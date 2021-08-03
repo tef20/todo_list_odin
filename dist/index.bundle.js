1ed7e0a7b730e9c17e86a04905fb9019d44d8457
Chris Telford <christelford@hotmail.co.uk>
2021-08-03 23:42:14 +0100

    setup gitignore, webpack, watch, start, sourcemaps, devServer

---
+++ dist/index.bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const h1 = document.createElement('h1');
+document.body.appendChild(h1);
+h1.textContent = 'hello';
+
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kb19saXN0X29kaW4vLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgaDEgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdoMScpO1xuZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChoMSk7XG5oMS50ZXh0Q29udGVudCA9ICdoZWxsbyc7XG5cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const h1 = document.createElement('h1');
+document.body.appendChild(h1);
+h1.textContent = 'hello';
+
