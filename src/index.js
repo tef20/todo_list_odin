3efa3057121a1f43f785b4a36bbf2b6e22c1adf3
Chris Telford <christelford@hotmail.co.uk>
2021-08-04 13:23:03 +0100

    clean for sidebar branch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './reset.css';
 import './style.css';
 
+const body = document.body;
+
+// const sidebar = document.createElement('div');
+// sidebar.className = 'side';
+
 const h1 = document.createElement('h1');
-document.body.appendChild(h1);
+body.appendChild(h1);
 h1.textContent = 'hello';
 
