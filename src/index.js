049afa8230d55acd125fd18e539c4143e04acdfa
Chris Telford <christelford@hotmail.co.uk>
2021-08-18 15:22:41 +0100

    Rough hardcoded html/css layout added
    - header,
    - sidebar
    -- checkbox display toggles on panel and projects list
    -- button to add new projects
    - view window
    -- listed todo items with checkbox task completion
    -- button to add new todo items

--- src/index.js
+++ src/index.js
@@ -3,10 +3,54 @@ import './style.css';
 
 const body = document.body;
 
-// const sidebar = document.createElement('div');
-// sidebar.className = 'side';
-
 const h1 = document.createElement('h1');
+h1.className = 'headerBar';
 body.appendChild(h1);
-h1.textContent = 'hello';
+h1.textContent = 'Main Header';
+
+// create nav 
+const nav = document.createElement('nav');
+nav.className = 'nav pagesection';
+body.appendChild(nav);
+
+const navList = document.createElement('ul');
+navList.className = 'navList';
+nav.appendChild(navList);
+
+// add nav items
+const navItem1 = document.createElement('li');
+navItem1.textContent = "nav1";
+navList.appendChild(navItem1);
+
+const navItem2 = document.createElement('li');
+navItem2.textContent = "nav2";
+navList.appendChild(navItem2);
+
+const navItem3 = document.createElement('li');
+navItem3.textContent = "nav3";
+navList.appendChild(navItem3);
+
+
+// create list section
+const tasksSection = document.createElement('section');
+tasksSection.className = 'tasks pagesection';
+body.appendChild(tasksSection);
+
+const tasksHeader = document.createElement('h2');
+tasksHeader.className = 'tasksHeader';
+tasksHeader.textContent = 'Task header';
+tasksSection.appendChild(tasksHeader);
+
+const tasksList = document.createElement('ul');
+tasksList.className = 'tasksList';
+tasksSection.appendChild(tasksList);
+
+const task1 = document.createElement('li');
+task1.textContent = "task1";
+tasksList.appendChild(task1);
+
+const task2 = document.createElement('li');
+task2.textContent = "task2";
+tasksList.appendChild(task2);
+
 
