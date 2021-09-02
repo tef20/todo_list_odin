dc12cff208abac2140ad7d81ff6fe7a3f9651b39
Chris Telford <christelford@hotmail.co.uk>
2021-09-02 17:38:56 +0100

    Redid todoLogic, converted hardcode to JS

--- src/index.js
+++ src/index.js
@@ -1,56 +1,191 @@
 import './reset.css';
 import './style.css';
+import { createTask, createTaskList, createProjectList } from './todoLogic.js';
 
 const body = document.body;
 
+const header = document.createElement('header');
+body.appendChild(header);
+
+const main = document.createElement('main');
+body.appendChild(main);
+
+// const footer = document.createElement('footer');
+
+// Header
 const h1 = document.createElement('h1');
 h1.className = 'headerBar';
-body.appendChild(h1);
 h1.textContent = 'Main Header';
+header.appendChild(h1);
 
-// create nav 
+// Nav //
 const nav = document.createElement('nav');
-nav.className = 'nav pagesection';
-body.appendChild(nav);
+// nav.className = 'nav pagesection';
+main.appendChild(nav);
+
+// create side toggle
+const sideToggle = document.createElement('input');
+sideToggle.type = 'checkbox';
+sideToggle.id = 'sideToggle';
+nav.appendChild(sideToggle);
+
+const sideToggleLabel = document.createElement('label');
+sideToggleLabel.htmlFor = 'sideToggle';
+sideToggleLabel.textContent = '>'
+nav.appendChild(sideToggleLabel);
 
 const navList = document.createElement('ul');
-navList.className = 'navList';
+// navList.className = 'navList';
 nav.appendChild(navList);
 
-// add nav items
-const navItem1 = document.createElement('li');
-navItem1.textContent = "nav1";
-navList.appendChild(navItem1);
+// Nav items 
+// Inbox
+const inbox = document.createElement('li');
+inbox.className = 'navHeading';
+inbox.textContent = "Inbox";
+navList.appendChild(inbox);
 
-const navItem2 = document.createElement('li');
-navItem2.textContent = "nav2";
-navList.appendChild(navItem2);
+const inboxIcon = document.createElement('i');
+inboxIcon.textContent = 'icon';
+inbox.insertAdjacentElement('afterbegin', inboxIcon);
 
-const navItem3 = document.createElement('li');
-navItem3.textContent = "nav3";
-navList.appendChild(navItem3);
+// Today
+const today= document.createElement('li');
+today.className = 'navHeading';
+today.textContent = "Today";
+navList.appendChild(today);
+
+const todayIcon = document.createElement('i');
+todayIcon.textContent = 'icon';
+today.insertAdjacentElement('afterbegin', todayIcon);
+
+// Upcoming
+const upcoming = document.createElement('li');
+upcoming.className = 'navHeading';
+upcoming.textContent = "Upcoming";
+navList.appendChild(upcoming);
+
+const upcomingIcon = document.createElement('i');
+upcomingIcon.textContent = 'icon';
+upcoming.insertAdjacentElement('afterbegin', upcomingIcon);
+
+// Projects toggle
+const projects = document.createElement('li');
+projects.id = 'projectsHeading';
+projects.className = 'navHeading';
+navList.appendChild(projects);
+
+const projectsToggle = document.createElement('input');
+projectsToggle.type = 'checkbox';
+projectsToggle.id = 'projectsToggle';
+projects.appendChild(projectsToggle);
+
+const projectsToggleLabel = document.createElement('label');
+projectsToggleLabel.htmlFor = 'projectsToggle';
+projectsToggleLabel.textContent = 'Projects \\/'
+projects.appendChild(projectsToggleLabel);
+
+const projectsToggleIcon = document.createElement('i');
+projectsToggleIcon.textContent = 'icon';
+projectsToggleLabel.insertAdjacentElement('afterbegin', projectsToggleIcon);
 
 
-// create list section
-const tasksSection = document.createElement('section');
-tasksSection.className = 'tasks pagesection';
-body.appendChild(tasksSection);
+// Projects
+const projectsList = document.createElement('ul');
+projectsList.className = 'navList';
+projectsList.id = 'projList';
+projects.appendChild(projectsList);
 
+// Project
+const project = document.createElement('ul');
+project.className = 'project';
+project.id = 'projID';
+project.textContent = 'Test Project';
+projectsList.appendChild(project);
+
+const projectIcon = document.createElement('i');
+projectIcon.textContent = 'icon';
+project.insertAdjacentElement('afterbegin', projectIcon);
+
+const deleteProjectIcon = document.createElement('i');
+deleteProjectIcon.textContent = 'delete';
+project.appendChild(deleteProjectIcon);
+
+// Add Project Button
+const addProjectButton = document.createElement('button');
+addProjectButton.id = 'addProject';
+addProjectButton.className = 'project';
+// addProjectButton.className = 'project';
+addProjectButton.textContent = 'Add project';
+projectsList.appendChild(addProjectButton);
+
+const addProjectIcon = document.createElement('i');
+addProjectIcon.textContent = 'icon';
+addProjectButton.insertAdjacentElement('afterbegin', addProjectIcon);
+
+// Viewer //
+// Section
+const tasksViewer = document.createElement('section');
+tasksViewer.id = 'taskViewer';
+main.appendChild(tasksViewer);
+
+// View Title
 const tasksHeader = document.createElement('h2');
 tasksHeader.className = 'tasksHeader';
 tasksHeader.textContent = 'Task header';
-tasksSection.appendChild(tasksHeader);
+tasksViewer.appendChild(tasksHeader);
 
+// Task List
 const tasksList = document.createElement('ul');
 tasksList.className = 'tasksList';
-tasksSection.appendChild(tasksList);
+tasksViewer.appendChild(tasksList);
 
-const task1 = document.createElement('li');
-task1.textContent = "task1";
-tasksList.appendChild(task1);
+// Task
+const task = document.createElement('li');
+task.className = 'task';
+tasksList.appendChild(task);
 
-const task2 = document.createElement('li');
-task2.textContent = "task2";
-tasksList.appendChild(task2);
+// Task Checkbox
+const taskCheckbox = document.createElement('input');
+taskCheckbox.id = `checkID${1}`;
+taskCheckbox.type = 'checkbox';
+task.appendChild(taskCheckbox);
 
+// Task Label
+const taskCheckboxLabel = document.createElement('label');
+taskCheckboxLabel.htmlFor = `checkID${1}`;
+taskCheckboxLabel.textContent = "Test task";
+task.appendChild(taskCheckboxLabel);
 
+// Task Description
+const taskDescription = document.createElement('span');
+taskDescription.textContent = 'description';
+task.appendChild(taskDescription);
+
+// Task Due Date
+const taskDueDate = document.createElement('span');
+taskDueDate.textContent = '01.01.1999';
+task.appendChild(taskDueDate);
+
+// Task Priority Icon
+const taskPriorityIcon = document.createElement('icon');
+taskPriorityIcon.className = 'checkIcon';
+taskPriorityIcon.textContent = 'medium';
+task.appendChild(taskPriorityIcon);
+
+// Task Delete Icon
+const taskDeleteIcon = document.createElement('icon');
+taskDeleteIcon.className = 'checkIcon';
+taskDeleteIcon.textContent = 'delete';
+task.appendChild(taskDeleteIcon);
+
+// Add Task Button
+const addTaskButton = document.createElement('button');
+addTaskButton.id = 'addTask';
+addTaskButton.className = 'task';
+addTaskButton.textContent = 'Add task';
+tasksList.appendChild(addTaskButton);
+
+const addTaskIcon = document.createElement('i');
+addTaskIcon.textContent = 'icon';
+addTaskButton.insertAdjacentElement('afterbegin', addTaskIcon);
--- src/todoLogic.js
+++ src/todoLogic.js
@@ -1,91 +1,156 @@
-const todoItems = (() => {
-    const _itemsList = [];
-    
-    function newTodoItem (name, description, due, priority, project) {
-        return {
-            name,
-            description, 
+// import from pubsub
+
+function createTask (name, description='description', due='none', priority='medium', project='unassigned', status='pending', id=genID.newID()) {
+    // subscribe to project name changes
+
+    return {
+        id,
+        'details': {
+            name, 
+            description,
             due,
-            priority, 
+            priority,
             project,
+            status,
         }
     }
+}
 
-    function listFilteredByProperty(property, filterValue) {
-        if (property) {
-            return _itemsList.filter((item) => {
-                return item[property] === filterValue;
+const genID = (() => {
+    // check for uniqueness / existing ID's in local storage 
+    // update local storage
+    let id = 0;
+
+    function newID() {
+        return id++;
+    }
+
+    return {newID};
+})();
+
+function createTaskList () {
+    const _tasksList = [];
+
+    function listIDs () {
+        return _tasksList.map(item => item.id)
+    }
+
+    function _checkID(id) {
+        return listIDs().includes(id);
+    }
+
+    function _getItemIndex(id) {
+        if (_checkID) {
+             _tasksList.forEach(item => {
+                if (item.id === id) {
+                    return _tasksList.indexOf(item);
+                }
             });
         }
     }
 
-    function updateProperty(item, property, newValue) {
-        if (_itemsList.includes(item)) {
-            _itemsList[item][property] = newValue;
+    function getItemByID(id) {
+        if (_checkID(id)) {
+            return _tasksList[_getItemIndex(id)];
         }
-        // publish update
     }
 
-    return {
-        ...adder(_itemsList),
-        ...lister(_itemsList),
-        ...remover(_itemsList),
-        listFilteredByProperty,
-        newTodoItem,
-        updateProperty,
-    }
-
-})();
-
-const projects = (() => {
-    const _projectsList = [];
-
-    function newProject(name) {
-        return {name};
-    }
-
-    function editProjName(oldName, newName) {
-        if (_projectsList.includes(oldName)) {
-            _projectsList[_projectsList.indexOf(oldName)] = newName;
+    function addItem(newTask) {
+        if (!(listIDs().includes(newTask.id))) {
+            _tasksList.push(newTask);
         }
-        // publish name change
+        // pubsub
+    } 
+
+    function removeItemByID(id) {
+        if (_checkID(id)) {
+            _tasksList.splice(_getItemIndex(id), 1);
+            // pubsub
+        }
+    }
+
+    function editItemDetails(id, newDetails) {
+        if (_checkID) {
+            getItemByID(id).details = {...newDetails}
+            // pubsub
+        }
+    }
+
+    function activeProjectsList() {
+        const activeProjects = [];
+
+        _tasksList.forEach(task => {
+            // checks ID uniqueness but what if not?
+            if (Object.keys(task.details).includes('project')) {
+                activeProjects.push(task.details.project);
+            }
+        });
+
+        return activeProjects;
     }
     
-    function sortProjects() {
-        _projectsList.sort();
-    }
-
     return {
-        ...adder(_projectsList),
-        ...lister(_projectsList),
-        ...remover(_projectsList),
-        editProjName,
-        newProject,
-        sortProjects,
-    }
-})();
-
-function adder(items) {
-    return {
-        add: (newItem) => items.push(newItem)
-        // publish ?
+        listIDs,
+        getItemByID,
+        addItem,
+        removeItemByID,
+        editItemDetails,
+        activeProjectsList,
     }
 }
 
-function remover(items) {
-    return {
-        remove: (itemToRemove) => items.splice(items.indexOf(itemToRemove), 1)
+
+function createProjectList () {
+    const _projectsList = new Set();
+
+    function addProject(name) {
+        return _projectsList.add(name);
     }
-        // publish ?
+
+    function removeProject(project) {
+        if (_projectsList.has(project)) {
+            _projectsList.delete(project);
+        }
+    }
+
+    function editProject(oldProject, newProject) {
+        if (_projectsList.has(oldProject)) {
+            _projectsList.delete(oldProject);
+            _projectsList.add(newProject);
+        }
+        // publish oldProject -> newProject change 
+    }
+    
+    function listProjects() {
+        let list = [..._projectsList].sort();
+        return list;
+    }
+
+    return {
+        addProject,
+        removeProject,
+        editProject,
+        listProjects,
+    }
 }
 
-function lister(items) {
-    return {
-        list: () => [...items]
-    }
-}
+// const taskList = createTaskList();
+// const task1 = createTask('task1');
+// const task2 = createTask('task2');
+// const task3 = createTask('task3', description='description', due='none', priority='medium', project='shopping');
+// taskList.addItem(task1);
+// taskList.addItem(task2);
+// taskList.addItem(task3);
+// const projects = createProjectList();
+// let activePs = taskList.activeProjectsList();
+// activePs.forEach(p => projects.addProject(p));
+// console.log(projects.listProjects());
+// console.log(taskList.listIDs())
+// console.log(taskList.removeItemByID(taskList.listIDs()[0]))
+// console.log(taskList.listIDs())
 
 export {
-    todoItems, 
-    projects
+    createTask,
+    createTaskList,
+    createProjectList,
 }
